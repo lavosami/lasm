3a5aae39dce34787da5935b38ae3a00c81d22a2f
lavosami <paramonovjegor@yandex.ru>
2024-02-29 20:41:04 +0300

    added changes

--- include/lasm.h
+++ include/lasm.h
@@ -1,5 +1,5 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef LASM_HEADER
+#define LASM_HEADER
 
 #define comSymb ';'
 
@@ -7,31 +7,31 @@
 #include <stdlib.h>
 
 typedef struct Lasm {
-   char* lable;
-   char* oper;
-   char* operand;
-   char* comment;
+  char* lable;
+  char* oper;
+  char* operand;
+  char* comment;
 } Lasm;
 
 char* substring(char* s, int p, int l) {
-   int c = 0;
-   char* sub = (char*)malloc(sizeof(char) * l);
-   while (c < l) {
-      sub[c] = s[p+c-1];
-      c++;
-   }
-   sub[c] = '\0';
-   return sub;
+  int c = 0;
+  char* sub = (char*)malloc(sizeof(char) * l);
+  while (c < l) {
+    sub[c] = s[p+c-1];
+    c++;
+  }
+  sub[c] = '\0';
+  return sub;
 }
 
 char* findComment(char* str) {
-   for (int i = 0; i < strlen(str); i++) {
-      if (str[i] == comSymb) {
-         return substring(str, i + 2, strlen(str)-i + 1);
-      }
-   }
+  for (int i = 0; i < strlen(str); i++) {
+    if (str[i] == comSymb) {
+      return substring(str, i + 2, strlen(str)-i + 1);
+    }
+  }
 
-   return "";
+  return "";
 }
 
 #endif
