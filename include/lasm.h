05be3ee918a6ba9f379c3732e6cb915d03c2a572
lavosami <paramonovjegor@yandex.ru>
2024-04-18 19:46:42 +0300

    made changes

--- include/lasm.h
+++ include/lasm.h
@@ -2,12 +2,12 @@
 #define LASM_HEADER
 
 #define MAX_WORDS 128
+#define COMMENT_CHAR ';'
 
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-const char* COMMENT_CHAR = ";";
 
 typedef struct Lasm {
   char* tag;
@@ -15,6 +15,7 @@ typedef struct Lasm {
   char* operand;
   char* comment;
 } Lasm;
+
 
 char** split(char* input) {
   // allocate memory for strings array
@@ -57,10 +58,10 @@ char** split(char* input) {
 
 void getComment(char* str, Lasm* model) {
   for (size_t i = 0; i < strlen(str); i++) {
-    if (COMMENT_CHAR[0] == str[i]) {
+    if (COMMENT_CHAR == str[i]) {
       // Allocate memory for model->comment
       model->comment = malloc(strlen(str) - i);
-      strncpy(model->comment, str + i + 1, strlen(str) - i - 1);
+      strncpy(model->comment, str + i + 1, strlen(str) - i - 2);
       // Ensure the string is null-terminated
       model->comment[strlen(str) - i - 1] = '\0';
       return; // Exit after getting the comment
@@ -69,6 +70,7 @@ void getComment(char* str, Lasm* model) {
   // If there's no comment, set it to an empty string
   model->comment = strdup("");
 }
+
 
 void getTag(char* str, Lasm* model) {
   char** words = split(str);
@@ -86,15 +88,16 @@ void getTag(char* str, Lasm* model) {
   model->tag = strdup("");
 }
 
+
 void getOperator(char* str, Lasm* model) {
   char** words = split(str);
-  if (strcmp(model->tag, words[0]) == 0 && strcmp(COMMENT_CHAR, words[1]) != 0) {
+  if (strcmp(model->tag, words[0]) == 0 && COMMENT_CHAR != words[1]) {
     model->operator = (char*)malloc((strlen(words[1]) + 1) * sizeof(char));
     strcpy(model->operator, words[1]);
     free(words);
     return;
   }
-  else if (strcmp(COMMENT_CHAR, words[0]) == 1) {
+  else if (COMMENT_CHAR != words[0]) {
     model->operator = malloc((strlen(words[0]) + 1) * sizeof(char));
     strcpy(model->operator, words[0]);
     free(words);
@@ -104,14 +107,18 @@ void getOperator(char* str, Lasm* model) {
   free(words);
 }
 
+
 void getOperand(char* str, Lasm* model) {
+    // Вызываем функцию split для токенизации входной строки
     char** words = split(str);
     
+    // Инициализируем операнд пустой строкой
     model->operand = strdup("");
     
+    // Находим индекс точки с запятой, обозначающей начало комментария
     int comment_index = -1;
     for (int i = 0; words[i] != NULL; i++) {
-        if (strcmp(words[i], COMMENT_CHAR) == 0) {
+        if (words[i] == ";") {
             comment_index = i;
             break;
         }
@@ -126,30 +133,33 @@ void getOperand(char* str, Lasm* model) {
         }
     }
     
+    // Конкатенируем слова между оператором и комментарием (если присутствует)
     if (operator_index != -1) {
-        // define begin and end index of operator
+        // Определяем начальный и конечный индексы для операнда
         int operand_start_index = operator_index + 1;
         int operand_end_index = (comment_index != -1) ? comment_index : operator_index + 1;
         
-        // memory allocation
+        // Выделяем память для операнда и конкатенируем слова
         size_t operand_length = 0;
         for (int i = operand_start_index; i < operand_end_index; i++) {
-            operand_length += strlen(words[i]) + 1; // +1 for space between the words
+            operand_length += strlen(words[i]) + 1; // +1 для пробела между словами
         }
-        model->operand = (char*)malloc((operand_length + 1) * sizeof(char));
+        model->operand = (char*)malloc((operand_length + 1) * sizeof(char)); // +1 для нулевого символа
         strcpy(model->operand, "");
         for (int i = operand_start_index; i < operand_end_index; i++) {
             strcat(model->operand, words[i]);
             strcat(model->operand, " ");
         }
-        model->operand[operand_length] = '\0';
+        model->operand[operand_length] = '\0'; // Убеждаемся, что строка операнда завершена нулевым символом
     }
     
+    // Освобождаем память, выделенную для токенизации строки на слова
     free(words);
 }
 
+
 void print(Lasm model) {
-  printf("Метка: %s, Оператор: %s, Операнд: %s, Комментарий: %s\n", model.tag, model.operator, model.operand, model.comment);
+  printf("Метка: %20s, Оператор: %20s, Операнд: %20s, Комментарий: %s\n", model.tag, model.operator, model.operand, model.comment);
 }
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,19 +1,46 @@
 #include "lasm.h"
 
-int main(int argc, char** argv) {
-  Lasm model;
-  char* str = "; Программа для демонстрации разбора строк";
-  getTag(str, &model);
-  getComment(str, &model);
-  getOperator(str, &model);
-  getOperand(str, &model);
-  print(model);
+void process(char* str, Lasm* model) {
+  getTag(str, model);
+  getComment(str, model);
+  getOperator(str, model);
+  getOperand(str, model);
+  print(*model);
 
   // Free allocated memory
-  free(model.tag);
-  free(model.comment);
-  free(model.operator);
-  free(model.operand);
+  free(model->tag);
+  free(model->comment);
+  free(model->operator);
+  free(model->operand);
+}
+
+
+int main(int argc, char** argv) {
+  Lasm model;
+  //   char* str = "            mov    ah, 9h";
+  // getTag(str, &model);
+  // getComment(str, &model);
+  // getOperator(str, &model);
+  // getOperand(str, &model);
+  // print(model);
+
+  // // Free allocated memory
+  // free(model.tag);
+  // free(model.comment);
+  // free(model.operator);
+  // free(model.operand);
   
+
+  FILE* file;
+  file = fopen(argv[1], "r");
+
+  char* str;
+
+  while(fgets(str, 512, file)) {
+    process(str, &model);
+  }
+
+  fclose(file);
+
   return 0;
 }
